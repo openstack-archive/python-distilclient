4af746ce39c769b92ccbc96562fea45f21e04a9f
Kiran_totad <kiran.totad@nectechnologies.in>
2017-07-24 12:33:49 +0530

    Replace six.iteritems() with .items()
    1.As mentioned in [1], we should avoid using six.iteritems to achieve
    iterators. We can use dict.items instead, as it will return iterators
    in PY3 as well. And dict.items/keys will more readable.
    2.In py2, the performance about list should be negligible, see the
    link [2].
    [1] https://wiki.openstack.org/wiki/Python3
    [2] http://lists.openstack.org/pipermail/openstack-dev/2015-June/066391.html
    
    Change-Id: I43d1d93817fa533fdfc68893008a2921eb09ddcf

--- distilclient/common/apiclient/base.py
+++ distilclient/common/apiclient/base.py
@@ -27,7 +27,6 @@ import abc
 import copy
 
 from oslo_utils import strutils
-import six
 from six.moves.urllib import parse
 
 from distilclient.common.apiclient import exceptions
@@ -303,7 +302,7 @@ class CrudManager(BaseManager):
 
     def _filter_kwargs(self, kwargs):
         """Drop null values and handle ids."""
-        for key, ref in six.iteritems(kwargs.copy()):
+        for key, ref in kwargs.copy()items():
             if ref is None:
                 kwargs.pop(key)
             else:
@@ -461,7 +460,7 @@ class Resource(object):
         return None
 
     def _add_details(self, info):
-        for (k, v) in six.iteritems(info):
+        for (k, v) in info.items():
             try:
                 setattr(self, k, v)
                 self._info[k] = v
